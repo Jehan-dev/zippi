58a6e6d29a8a56ce4b6d14d9bfccbbd539a3572e
Jehan-dev <evgenbuz@inbox.ru>
2019-09-17 22:45:10 +0300

    added section: sign, blog, contact

--- app/js/main.js
+++ app/js/main.js
@@ -15,4 +15,21 @@ $(function () {
         autoplaySpeed: 5000,
     })
 
+    $('.life__inner').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        fade: false,
+        asNavFor: '.life__nav',
+        nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+        prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+    }),
+    $('.life__nav').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        asNavFor: '.life__inner',
+        centerMode: true,
+        focusOnSelect: true,
+    });
+
 })
